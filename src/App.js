973f470bb6f6834e404f7789a6ba84b48203e024
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2023-06-14 18:12:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -23,3 +23,27 @@ function App() {
 }
 
 export default App;
+
+
+
+// file structure
+
+/*
+javascript-calculator/
+|-- node_modules/
+|-- public/
+|   |-- index.html
+|-- src/
+|   |-- components/
+|   |   |-- Button.js
+|   |   |-- Display.js
+|   |   |-- ButtonPanel.js
+|   |   |-- Calculator.js
+|   |-- App.js
+|   |-- App.css
+|   |-- index.js
+|-- package.json
+|-- package-lock.json
+|-- README.m
+
+*/
